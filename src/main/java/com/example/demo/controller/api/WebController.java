833985be1ff71c62d40b5b685143c97d25d57d4c
oyafuso <oyafuso_taka@r-net.co.jp>
2020-04-09 17:38:12 +0900

    返すJSONの値にstatus,massage,dataを追加

--- src/main/java/com/example/demo/controller/api/WebController.java
+++ src/main/java/com/example/demo/controller/api/WebController.java
@@ -38,43 +38,72 @@ public class WebController {
 	 */
 	@RequestMapping(method = RequestMethod.GET)
 	public String SyainInfo(@RequestParam("UserId") String userId){
+		// status番号
+		String statusNum;
 		
 		// WebValidation呼び出し(数字チェック)
 		boolean valChack = WebValidation.numberCheck(userId);
 		
+		//　リストにデータが入っていく
+		List<JSONObject> objList = new ArrayList<JSONObject>();
+		// 出力用(status,message,data) JSON形式オブジェクト
+		JSONObject outputObj =  new JSONObject();
+		//　社員データ用(社員番号,名前,年齢,部署)　JSON形式オブジェクト
+		JSONObject empObj =  new JSONObject();
+		
 		// URLのパラメータが数字の場合
 		if(valChack == true) {
+			// status番号
+			statusNum = "200";
+			
 			// Stringをint型へ
 			int userIdInt = Integer.parseInt(userId);
 			
 			// 引数にIDを入力し、データを取得
 			List<SpringStudyEntity> emp = springStudyRepository.findById(userIdInt);
 			
-			//　リストにデータが入っていく
-			List<JSONObject> objList = new ArrayList<JSONObject>();
-			//　JSON用のオブジェクト生成
-			JSONObject obj =  new JSONObject();
-			
-			// ListにDBから取得したデータを入れる
-			obj.put("社員番号",emp.get(0).getEmployeeId().toString());
-		    obj.put("名前", emp.get(0).getEmployeeName().toString());
-		    obj.put("年齢", emp.get(0).getAge().toString());
-		    obj.put("所属", emp.get(0).getDepartment().toString());
-		    objList.add(obj);
-    
+			// empObjにDBから取得したデータを入れる
+			empObj.put("社員番号", emp.get(0).getEmployeeId().toString());
+			empObj.put("名前", emp.get(0).getEmployeeName().toString());
+			empObj.put("年齢", emp.get(0).getAge().toString());
+			empObj.put("所属", emp.get(0).getDepartment().toString());
+		    
+		    // status番号を入れる(成功)
+		    outputObj.put("status",statusNum);
+		    // messageを入れる
+		    outputObj.put("messeage",messageSource.getMessage(statusNum, null, Locale.JAPAN));
+		    // 取得した社員データを入れる
+		    outputObj.put("data", empObj);
+		    
+		    // Listにデータ(status,massage,data)が入ったオブジェクトを入れる
+		    objList.add(outputObj);
+		    
 		    // JSON型のListを返す
 		    return objList.get(0).toString();
 		}
 		// URLのパラメータが数字ではないとき
 		else {
+			// status番号
+			statusNum = "300";
 			/*
 			 * ※メッセージプロパティを使うには
 			 * 「application.properties」に設定を記入
 			 * 「messages_ja.properties」にキーと表示したい文字を記入
 			 * 「messages.properties」のファイルを作成(中身は空)  
 			*/
-			// messages_ja.propertiesのキーと場所を指定して、メッセージプロパティの値を返す
-			return messageSource.getMessage("numberCheck", null, Locale.JAPAN);
+			
+			// status番号を入れる(失敗)
+		    outputObj.put("status",statusNum);
+		    // messageを入れる
+		    outputObj.put("messeage",messageSource.getMessage(statusNum, null, Locale.JAPAN));
+		    // 取得した社員データを入れる
+		    outputObj.put("data", empObj);
+		    
+		    // Listにデータ(status,massage)が入ったオブジェクトを入れる
+		    objList.add(outputObj);
+		    
+		    // JSON型のListを返す
+		    return objList.get(0).toString();
 		}
 	}	
 }
